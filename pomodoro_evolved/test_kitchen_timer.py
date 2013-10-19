aa018ff90f43b96b1a96238ce1fa89d1fb96ca7f
Douglas Hellinger <doug.hellinger@hotmail.com>
2013-10-19 22:43:23 +0800

    2nd Timer test case passing.

--- pomodoro_evolved/test_kitchen_timer.py
+++ pomodoro_evolved/test_kitchen_timer.py
@@ -4,13 +4,22 @@ class KitchenTimer(object):
     
     def __init__(self):
         self.state = "Stopped"
+        
+    def stop(self):
+        pass
+    
 
-class Test(unittest.TestCase):
+class TestTimer(unittest.TestCase):
 
+    def setUp(self):
+        self.timer = KitchenTimer()        
 
     def test_afterInitialisationTimerIsStopped(self):
-        timer = KitchenTimer()
-        self.assertEqual("Stopped", timer.state)
+        self.assertEqual("Stopped", self.timer.state)
+        
+    def test_stoppingAStoppedTimerDoesNothingToItsState(self):
+        self.timer.stop()
+        self.assertEqual("Stopped", self.timer.state)
 
 
 if __name__ == "__main__":
