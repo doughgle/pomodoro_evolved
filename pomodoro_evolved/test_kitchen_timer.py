f7121822be6358675f437500b7b03b36557eb848
Douglas Hellinger <doug.hellinger@hotmail.com>
2013-10-19 23:25:31 +0800

    3rd pomodoro. + Tested some state transitions. + Tested some null transitions.

--- pomodoro_evolved/test_kitchen_timer.py
+++ pomodoro_evolved/test_kitchen_timer.py
@@ -1,25 +1,67 @@
 import unittest
 
-class KitchenTimer(object):
+idle = "idle"
+running = "running"
+stopped = "stopped"
+
+class Timer(object):
     
     def __init__(self):
-        self.state = "Stopped"
+        self.state = idle
         
+    def start(self):
+        if self.state == idle:
+            self.state = running
+    
     def stop(self):
+        if self.state == running:
+            self.state = stopped
+            
+    def reset(self):
         pass
+    
     
 
 class TestTimer(unittest.TestCase):
 
-    def setUp(self):
-        self.timer = KitchenTimer()        
+    def assertIdle(self):
+        return self.assertEqual(idle, self.timer.state)
 
-    def test_afterInitialisationTimerIsStopped(self):
-        self.assertEqual("Stopped", self.timer.state)
+    def assertStopped(self):
+        return self.assertEqual(stopped, self.timer.state)
+
+    def setUp(self):
+        self.timer = Timer()        
+
+
+    def test_afterInitialisation_TimerIsIdle(self):
+        self.assertEqual(idle, self.timer.state)
         
-    def test_stoppingAStoppedTimerDoesNothingToItsState(self):
+    def test_stoppingFromIdle_DoesNothing(self):
         self.timer.stop()
-        self.assertEqual("Stopped", self.timer.state)
+        self.assertEqual(idle, self.timer.state)
+        
+    def test_resettingFromIdle_DoesNothing(self):
+        self.timer.reset()
+        self.assertIdle()
+        
+    def test_afterStartingFromIdle_TimerIsRunning(self):
+        self.timer.start()
+        self.assertEqual(running, self.timer.state)
+        
+    def test_afterStoppingARunningTimer_timerIsStopped(self):
+        self.timer.start()
+        self.timer.stop()
+        self.assertStopped()
+        
+    def test_startingFromStopped_DoesNothing(self):
+        self.timer.start()
+        self.timer.stop()
+        self.timer.start()
+        self.assertStopped()
+        
+        
+        
 
 
 if __name__ == "__main__":
