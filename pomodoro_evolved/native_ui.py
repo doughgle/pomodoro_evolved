d47063363d87561239f59d7f3229f1a54a2349d7
Douglas Hellinger <doug.hellinger@hotmail.com>
2013-11-02 09:07:07 +0800

    Start button added. Time remaining shown before starting.

--- pomodoro_evolved/native_ui.py
+++ pomodoro_evolved/native_ui.py
@@ -5,16 +5,21 @@ from datetime import timedelta
 
 class NativeUI(tk.Tk):
     def __init__(self):
+        self.pomodoro = Pomodoro(self.whenTimeup, durationInMins=0.1)
         tk.Tk.__init__(self)
         self.clockFont = tkFont.Font(family="Helvetica", size=18)
-        self.label = tk.Label(self, text="", width=10, font=self.clockFont)
+        self.label = tk.Label(self, text=str(timedelta(seconds=self.pomodoro.timeRemaining)), width=10, font=self.clockFont)
         self.label.pack()
-        self.pomodoro = Pomodoro(self.whenTimeup, durationInMins=0.1)
-        self.pomodoro.start()
-        self.update_display(self.pomodoro.timeRemaining)
+        self.startButton = tk.Button(self, text ="Start", command=self.onStart)
+        self.startButton.pack()
 
     def whenTimeup(self):
         print "time's up!"
+        
+    def onStart(self):
+        self.pomodoro.start()
+        self.update_display(self.pomodoro.timeRemaining)
+        print "started!"
         
     def update_display(self, remaining=None):
         self.label.configure(text=str(timedelta(seconds=self.pomodoro.timeRemaining)))
