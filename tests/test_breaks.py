a3b2ff64c9c1692953e4c2a922c46ffd19236d32
Douglas Hellinger <doug.hellinger@hotmail.com>
2013-11-04 11:54:10 +0800

    Changed refs to shortBreak to restBreak in tests.

--- tests/test_breaks.py
+++ tests/test_breaks.py
@@ -3,69 +3,65 @@ from rest_break import Break
 from rest_break import AlreadySkippedError, CannotSkipOnceStarted, BreakAlreadyStarted
 from time import sleep
 
-class TestShortBreak(unittest.TestCase):
+class TestRestBreak(unittest.TestCase):
 
     def setUp(self):
-        self.shortBreak = Break(self.whenTimeup)
+        self.restBreak = Break(self.whenTimeup)
                 
     def whenTimeup(self):
         self.timeUp = True
 
     def assertRunning(self):
-        return self.assertTrue(self.shortBreak.isRunning())
+        return self.assertTrue(self.restBreak.isRunning())
 
     def assertPostCondition_onceSkippedCanNeverBeRunning(self):
-        return self.assertNotEqual(self.shortBreak.isRunning(), self.shortBreak.wasSkipped())    
+        return self.assertNotEqual(self.restBreak.isRunning(), self.restBreak.wasSkipped())    
     
-    def test_afterCreationShortBreakIsNotRunning(self):
-        self.assertFalse(self.shortBreak.isRunning())
+    def test_afterCreationBreakIsNotRunning(self):
+        self.assertFalse(self.restBreak.isRunning())
         
-    def test_afterCreationShortBreakIsNotSkipped(self):
-        self.assertFalse(self.shortBreak.wasSkipped())        
+    def test_afterCreationBreakIsNotSkipped(self):
+        self.assertFalse(self.restBreak.wasSkipped())        
     
     def test_afterCreation_timeRemainingInSecondsIsEquivalentToDurationInMinutes(self):
-        self.assertEqual((self.shortBreak._durationInMins * 60), self.shortBreak.timeRemaining)
+        self.assertEqual((self.restBreak._durationInMins * 60), self.restBreak.timeRemaining)
             
     def test_afterSkipping_wasSkippedReturnsTrue(self):
-        self.shortBreak.skip()
-        self.assertTrue(self.shortBreak.wasSkipped())
+        self.restBreak.skip()
+        self.assertTrue(self.restBreak.wasSkipped())
         self.assertPostCondition_onceSkippedCanNeverBeRunning()
         
-    def test_afterSkipping_startingAShortBreakIsAnAlreadySkippedError(self):
-        self.shortBreak.skip()
-        self.assertRaises(AlreadySkippedError, self.shortBreak.start)
+    def test_afterSkipping_startingABreakIsAnAlreadySkippedError(self):
+        self.restBreak.skip()
+        self.assertRaises(AlreadySkippedError, self.restBreak.start)
         self.assertPostCondition_onceSkippedCanNeverBeRunning()
     
     def test_afterStarting_isRunningReturnsTrue(self):
-        self.shortBreak.start()
+        self.restBreak.start()
         self.assertRunning()
-        self.assertFalse(self.shortBreak.wasSkipped())
+        self.assertFalse(self.restBreak.wasSkipped())
         
     def test_afterStarting_skippingABreakIsACannotSkipBreakOnceStarted(self):
-        self.shortBreak.start()
-        self.assertRaises(CannotSkipOnceStarted, self.shortBreak.skip)
+        self.restBreak.start()
+        self.assertRaises(CannotSkipOnceStarted, self.restBreak.skip)
         
     def test_startingABreakThatIsAlreadyStartedIsAnAlreadyStartedException(self):
-        self.shortBreak.start()
-        self.assertRaises(BreakAlreadyStarted, self.shortBreak.start)
+        self.restBreak.start()
+        self.assertRaises(BreakAlreadyStarted, self.restBreak.start)
 
     def test_afterStarting_BreakCallsBackWhenTimesUp(self):
         self.timeUp = False
-        self.shortBreak = Break(self.whenTimeup, durationInMins=0.001)
-        self.shortBreak.start()
+        self.restBreak = Break(self.whenTimeup, durationInMins=0.001)
+        self.restBreak.start()
         sleep(0.1)
         self.assertTrue(self.timeUp)
         
     def test_afterStarting_breakShouldNotCallBackBeforeTimesup(self):
         self.timeUp = False
-        self.shortBreak = Break(self.whenTimeup, durationInMins=1)
-        self.shortBreak.start()
+        self.restBreak = Break(self.whenTimeup, durationInMins=1)
+        self.restBreak.start()
         sleep(0.05)
         self.assertFalse(self.timeUp)
-
-        
-        
-    
 
 if __name__ == "__main__":
     unittest.main()
