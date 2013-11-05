c60f4bb04a1873d57a7383165b2945b31133774d
Douglas Hellinger <doug.hellinger@hotmail.com>
2013-11-05 14:39:04 +0800

    CAPITALISED class level constants.

--- pomodoro_evolved/rest_break.py
+++ pomodoro_evolved/rest_break.py
@@ -10,58 +10,58 @@ class BreakAlreadyTerminated(Exception): pass
 
 class Break(object):
     
-    idle =      "idle"
-    running =   "running"
-    skipped =   "skipped"
-    stopped =   "stopped"
-    timeup =    "timeup"
+    IDLE =      "IDLE"
+    RUNNING =   "RUNNING"
+    SKIPPED =   "SKIPPED"
+    STOPPED =   "STOPPED"
+    TIMEUP =    "TIMEUP"
     
     def __init__(self, whenTimeup, durationInMins=5):
-        self._state = self.idle
+        self._state = self.IDLE
         self._durationInMins = durationInMins
         self._userWhenTimeup = whenTimeup
         self._timer = KitchenTimer()
             
     def skip(self):
-        if self._state == self.idle:
-            self._state = self.skipped
+        if self._state == self.IDLE:
+            self._state = self.SKIPPED
         else:
             raise CannotSkipOnceStarted()
     
     def start(self):
-        if self._state == self.idle:
+        if self._state == self.IDLE:
             self._timer.start(minsToSecs(self._durationInMins), self._whenTimeup)
-            self._state = self.running
-        elif self._state == self.skipped:
+            self._state = self.RUNNING
+        elif self._state == self.SKIPPED:
             raise BreakAlreadySkipped()
-        elif self._state == self.running:
+        elif self._state == self.RUNNING:
             raise BreakAlreadyStarted()
         else:
             raise BreakAlreadyTerminated()
             
     def stop(self):
-        if self._state == self.skipped:
+        if self._state == self.SKIPPED:
             raise BreakAlreadySkipped()
-        elif self._state != self.running:
+        elif self._state != self.RUNNING:
             raise BreakNotStarted()
         else:
             self._timer.stop()
-            self._state = self.stopped
+            self._state = self.STOPPED
             
     def isRunning(self):
-        return self._state == self.running
+        return self._state == self.RUNNING
     
     def wasSkipped(self):
-        return self._state == self.skipped
+        return self._state == self.SKIPPED
 
     @property
     def timeRemaining(self):
-        if self._state == self.idle:
+        if self._state == self.IDLE:
             return minsToSecs(self._durationInMins)
         else:
             return ceil(self._timer.timeRemaining)
     
     def _whenTimeup(self):
-        self._state = self.timeup
+        self._state = self.TIMEUP
         if callable(self._userWhenTimeup):
             self._userWhenTimeup()
