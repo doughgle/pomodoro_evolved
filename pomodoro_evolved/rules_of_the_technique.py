ea515b1aed1ed760206d7fd119ea8c947f966beb
Douglas Hellinger <doug.hellinger@hotmail.com>
2013-11-09 22:10:26 +0800

    Documented the purpose of the class.

--- pomodoro_evolved/rules_of_the_technique.py
+++ pomodoro_evolved/rules_of_the_technique.py
@@ -1,9 +1,17 @@
 
 class RulesOfTheTechnique(object):
-    
+    '''
+    Encapsulates the following rules of the Pomodoro technique:
+      Begin with a Pomodoro.
+      After a Pomodoro, take a rest break.
+      After a rest break, start a new Pomodoro.
+      Usually a Pomodoro is followed by a short break, except when the long break interval is reached.
+      After a decided number of Pomodoros (default=4) a long break will replace the short break.
+    '''
+      
     EVERY_FOUR_POMODOROS = 4
     
-    def __init__(self, pomodoroCls, shortBreakCls, longBreakCls, longBreakInterval=EVERY_FOUR_POMODOROS):
+    def __init__(self, pomodoroCls, shortBreakCls, longBreakCls, longBreakInterval=EVERY_FOUR_POMODOROS):        
         self.Pomodoro = pomodoroCls
         self.ShortBreak = shortBreakCls
         self.LongBreak = longBreakCls
