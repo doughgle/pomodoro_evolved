2e0916fa5a0257ef0b8a05f2cd05c2c6dcc68a18
Douglas Hellinger <doug.hellinger@hotmail.com>
2014-01-02 14:01:12 +0800

    Shorten default Pomodoro duration for faster system testing. Reconciled whitespace.

--- pomodoro_evolved/native_ui.py
+++ pomodoro_evolved/native_ui.py
@@ -42,7 +42,7 @@ class NativeUI(tk.Tk):
                 else:
                     self.timer = ShortBreak(self.whenTimeup, durationInMins=self._shortBreakDurationInMins)
                     self.timerName = "Short Break"
-        
+         
         self.title(self.timerName)
         self.clock.configure(text=str(timedelta(seconds=self.timer.timeRemaining)))
         self.startStopButton.configure(text="Start", command=self.onStart)
--- tests/test_system_with_native_ui.py
+++ tests/test_system_with_native_ui.py
@@ -2,5 +2,5 @@ from native_ui import NativeUI
 
 if __name__ == "__main__":
     # configure settings for faster system testing
-    app = NativeUI(pomodoroDurationInMins=0.05, shortBreakDurationInMins=0.02, longBreakDurationInMins=0.1)
+    app = NativeUI(pomodoroDurationInMins=0.02, shortBreakDurationInMins=0.02, longBreakDurationInMins=0.1)
     app.mainloop()
