52e69b7ad16f352c9fa628ac1b10e6a531106716
Douglas Hellinger <doug.hellinger@hotmail.com>
2014-03-30 13:11:14 +0800

    Hacked a web UI with 25 minute timer and Start/Stop functionality.

---
+++ script.js
@@ -0,0 +1,43 @@
+var seconds = 1500;
+
+$(document).ready(function(){
+	
+	$('#start').click(function() {
+		start();
+		$("#start").attr("disabled", "disabled");
+		console.log("started!")
+	});
+
+	$('#stop').click(function() {
+		stop();
+		$("#start").removeAttr("disabled");
+		console.log("stopped!")
+	});
+	
+});
+ 
+function start() {
+	countdownTimer = setInterval('secondPassed()', 1000);
+}
+
+function stop() {
+	clearInterval(countdownTimer);
+}
+
+function secondPassed() {
+	  var minutes = Math.round((seconds - 30)/60),
+	      remainingSeconds = seconds % 60;
+	  
+	  if (remainingSeconds < 10) {
+	    remainingSeconds = "0" + remainingSeconds;  
+	  }
+	  
+	  document.getElementById('countdown').innerHTML = minutes + ":" + remainingSeconds;
+	  if (seconds == 0) {
+	    clearInterval(countdownTimer);
+	    document.getElementById('countdown').innerHTML = "00:00";
+	  } else {
+	    seconds--;
+	  }
+}
+
